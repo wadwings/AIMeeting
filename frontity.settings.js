64d61e59f960abad826b9ffcc50f553c4c68d763
wings <wingswad@gmail.com>
2021-05-19 16:10:35 +0800

    fix: 🐛 merge wings_dev

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,54 +1,20 @@
 const settings = {
-<<<<<<< HEAD
-  "name": "frontity",
-  "state": {
-    "frontity": {
-<<<<<<< HEAD
-      "url": "http://wp.dedsec.site",
-=======
-      "url": "https://tx.dedsec.site",
->>>>>>> wings_dev
-=======
   "name": "aimeeting",
   "state": {
     "frontity": {
       "url": "https://tx.dedsec.site",
->>>>>>> 1036f6ff7e629dcf41952c2db6868c82cd19a4ca
       "title": "Test Frontity Blog",
       "description": "WordPress installation for Frontity development"
     }
   },
   "packages": [
     {
-<<<<<<< HEAD
-      name: "my-first-theme"
-=======
       name: "aiMeeting"
->>>>>>> 1036f6ff7e629dcf41952c2db6868c82cd19a4ca
     },
     {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-<<<<<<< HEAD
-<<<<<<< HEAD
-          "url": "http://wp.dedsec.site"
-=======
-          "url": "https://tx.dedsec.site",
-          "postTypes": [
-            {
-              type: "person",
-              endpoint: "person",
-            },            
-            {
-              type: "profile",
-              endpoint: "profile",
-              archive: "/profile"
-            }
-          ]
->>>>>>> wings_dev
-        }
-=======
           "url": "https://tx.dedsec.site",
           'postTypes': [
             {
@@ -80,7 +46,6 @@ const settings = {
             },
           ]
         },
->>>>>>> 1036f6ff7e629dcf41952c2db6868c82cd19a4ca
       }
     },
     "@frontity/tiny-router",
