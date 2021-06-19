b957afea1a2338175b7ad779a7d502b76b116709
wings <wingswad@gmail.com>
2021-06-19 20:46:08 +0800

    fix: 🐛 delete everything

--- packages/aiMeeting/src/components/index.js
+++ packages/aiMeeting/src/components/index.js
@@ -3,26 +3,22 @@ import PC from './PC/root'
 import { connect, Global, css, styled } from "frontity";
 import React, {useState} from 'react'
 const root = () => {
-	const [isPC, setIsPC] = useState(false)
-	return(
-		<>
-			<Global styles={css`
-				@media screen and (max-width: 769px) {
-					#PC {
-						display: none;
-					}
-				}
-				
-				@media screen and (min-width: 768px) {
-					#mobile {
-						display: none;
-					}
-				}
-			`}></Global>
-			<PC />
-			<Mobile />
-		</>
-	)
+	return<Main>
+		<div>2021中国光谷人工智能大会暨企业家高峰论坛</div>
+		<A href='http://aic.hust.edu.cn'>【点击进入】</A>
+	</Main>
 }
+
+const Main = styled.div`
+	display: flex;
+	height: 100vh;
+	flex-direction: column;
+	align-items: center;
+	justify-content:center;
+`
+
+const A = styled.a`
+	color: black;
+`
 
 export default root;
